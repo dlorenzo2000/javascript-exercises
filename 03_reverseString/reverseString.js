dafccd1449ef9b0d4d5606822939dd696071b1f2
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-21 14:48:12 -0500

    Solution to problem.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = '';
 
+    const wordArray = word.split('');
+    for(let i = wordArray.length - 1; i >= 0; i--)
+        reversedWord += wordArray[i]; 
+
+    return reversedWord;
 };
 
 // Do not edit below this line
