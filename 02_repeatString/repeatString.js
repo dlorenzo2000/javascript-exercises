e6dacbe09bfec57a9261a4a4e26af8df18ba9deb
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-21 14:34:36 -0500

    Solved solution.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterator) {
+    let newString = '';
+    if(iterator < 0)
+        return 'ERROR';
+    for(let i = 0; i < iterator; i++)
+        newString += string;
+    return newString;
 };
 
 // Do not edit below this line
