e799902c9be707b4ec51ddf81a051c4d3604f07f
dlorenzo2000 <deanlorenzo@gmail.com>
2023-04-22 11:13:55 -0500

    Solved problem.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+// Sat Apr 22/23 1046hrs
 
+const leapYears = function(year) {
+    if(!Number.isInteger(year) || year < 0)
+        return 'ERROR';
+
+    let isLeapYear = false; 
+
+    if((year % 100 != 0 || year % 400 === 0) && year % 4 === 0)
+        isLeapYear = true; 
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
